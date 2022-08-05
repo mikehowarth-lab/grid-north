a91be0b46a2214ee09ce2224fb6c6dc4eb05c1da
Mike Howarth <hey@mikehowarth.co.uk>
2022-08-05 10:53:33 +0100

    Re-organisation in to a standard layout

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Link from "next/link";
+import { getDatabase } from "../lib/notion";
+import { Text } from "./[id].js";
+import styles from "./index.module.css";
+
+import Layout from "../components/Layout";
+
+export const databaseId = process.env.NOTION_DATABASE_ID;
+
+  
+const About = () => {
+  return (
+    <div>
+        <h1>About</h1>
+        <blockquote>Grid north is a navigational phrase used to refer to the northward on a grid lines in a map projection. Grid north contrasts the true north and the magnetic north in that it is aligned to grid lines and always points upwards on a map.</blockquote>
+    </div>
+  )
+}
+  
+export default About
