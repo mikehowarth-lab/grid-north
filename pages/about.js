00b380b7b1571540e2f98c3f26a8cbcf868ebb61
Mike Howarth <hey@mikehowarth.co.uk>
2022-08-11 12:56:47 +0100

    Added newsletter footer

--- pages/about.js
+++ pages/about.js
@@ -14,10 +14,10 @@ const About = () => {
     <div>
         <h1>Grid North</h1>
         <blockquote>Grid north is a navigational phrase used to refer to the northward on a grid lines in a map projection. Grid north contrasts the true north and the magnetic north in that it is aligned to grid lines and always points upwards on a map.</blockquote>
-        <p>Ever felt as though you weren't travelling on the right path and weren't living true to yourself?</p>
-        <p>In a noisy world full of distractions its easy to lose sight of your sense of purpose.</p>
-        <p>Grid North is an exploration in to creating a better future.</p>
-        <p>A place to come out of the shadows and lean in to the skills and knowledge you have to create a more purposeful future.</p>
+        <p>What happens when middle age slowly catches up on you?</p>
+        <p>Inertia is hard to overcome, and in a noisy world full of distractions its easy to lose sight of your sense of purpose.</p>
+        <p>Grid North is a personal exploration in to how and what I want my future to be.</p>
+        <p>How I connect my skills and interests with the lure of a full time employed job and the pursuit of financial independence through indie hacking.</p>
     </div>
   )
 }
